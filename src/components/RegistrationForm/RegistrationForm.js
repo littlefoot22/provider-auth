31caffbab2700d20c74ef08b78081d7dd9239007
buck20two <buck.20.two@gmail.com>
2020-06-30 22:28:43 -0400

    init reg page

--- src/components/RegistrationForm/RegistrationForm.js
+++ src/components/RegistrationForm/RegistrationForm.js
@@ -1,6 +1,64 @@
 
 import React, {useState} from 'react';
+import axios from 'axios';
+
 function RegistrationForm(props) {
+    const API_BASE_URL = "http://127.0.0.1:8000/"
+    const [state , setState] = useState({
+        email : "",
+        password : ""
+    })
+    const handleChange = (e) => {
+        const {id , value} = e.target   
+        setState(prevState => ({
+            ...prevState,
+            [id] : value
+        }))
+    }
+    const handleSubmitClick = (e) => {
+        //e.preventDefault();
+        sendDetailsToServer()    
+
+        if(state.password === state.confirmPassword) {
+            console.log("asdasdasadas")
+
+            sendDetailsToServer()    
+        } else {
+            //props.showError('Passwords do not match');
+        }
+    }
+    const sendDetailsToServer = () => {
+        console.log("asdasdasadas")
+
+        if(state.email.length && state.password.length) {
+            //props.showError(null);
+            const payload={
+                "email":state.email,
+                "password":state.password,
+            }
+            console.log("asdasdasadas")
+
+            axios.post(API_BASE_URL+'users', payload)
+                .then(function (response) {
+                    if(response.data.code === 200){
+                        setState(prevState => ({
+                            ...prevState,
+                            'successMessage' : 'Registration successful. Redirecting to home page..'
+                        }))
+                        //redirectToHome();
+                        props.showError(null)
+                    } else{
+                        props.showError("Some error ocurred");
+                    }
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });    
+        } else {
+            props.showError('Please enter valid username and password')    
+        }
+        
+    }
   return(
         <div className="card col-12 col-lg-4 login-card mt-2 hv-center">
             <form>
@@ -11,6 +69,8 @@ function RegistrationForm(props) {
                        id="email" 
                        aria-describedby="emailHelp" 
                        placeholder="Enter email"
+                       value={state.email}
+                       onChange={handleChange}
                 />
                 <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
                 </div>
@@ -20,6 +80,8 @@ function RegistrationForm(props) {
                         className="form-control" 
                         id="password" 
                         placeholder="Password"
+                        value={state.password}
+                        onChange={handleChange} 
                     />
                 </div>
                 <div className="form-group text-left">
@@ -33,6 +95,7 @@ function RegistrationForm(props) {
                 <button 
                     type="submit" 
                     className="btn btn-primary"
+                    onClick={handleSubmitClick}
                 >
                     Register
                 </button>
